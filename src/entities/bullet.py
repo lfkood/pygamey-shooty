c4470003a7125ab255fc77134134dfa14c8acb90
lfkood <152791020+lfkood@users.noreply.github.com>
2025-04-21 11:42:16 +0300

    Added rotation and a sprite for bullets (issue #2)

--- src/entities/bullet.py
+++ src/entities/bullet.py
@@ -5,12 +5,13 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, position, rotation):
         super().__init__()
         self.rotation = float(rotation)
-        self.image = pygame.Surface((5, 10))
-        self.image.fill((255, 255, 255))
+        self.image = pygame.image.load("assets/bul.png").convert_alpha()
         self.rect = self.image.get_rect(center=position)
         self.position = Vector2(position)
         self.speed = 500
-
+        
+        self.image = pygame.transform.rotate(self.image, self.rotation-90)
+        self.image = pygame.transform.scale(self.image, (20, 20))
         self.velocity = Vector2(0, -1).rotate(-self.rotation+90) * self.speed
 
     def update(self, dt):
